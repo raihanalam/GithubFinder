d40e3119eef5138e11d424c29113f94e261e4e94
raihanalam <raihanhridoybd@gmail.com>
2021-07-02 15:58:00 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let searchBtn = document.querySelector("#searchBtn");
+let searchUser = document.querySelector('#searchUser');
+
+let ui = new UI();
+
+searchBtn,addEventListener('click',(e)=>{
+    let userText = searchUser.value;
+    if(userText != ''){
+        fetch(`https://api.github.com/users/octocat/${userText}`)
+        .then(result => result.json())
+        .then(data => {
+            console.log(data);
+            if(data.message == 'Not Found'){
+                ui.showAlert("User Not Found!","alert alert-danger");
+            }
+            else{
+               ui.showProfile(data);
+            }
+        })
+    }
+    else{
+        ui.clearProfile();
+    }
+});
---
+++ js/ui.js
@@ -0,0 +1,60 @@
+class UI{
+    constructor(){
+        this.profile = document.querySelector('#profile');
+    }
+
+    showProfile(user){
+        this.clearAlert();
+        this.profile.innerHTML = `
+        <div class = "card card-body mb-3">
+            <div class="row">
+                <div class="col-md-3">
+                    <img class="img-fluid mb-2" src="${user.avatar_url}">
+                    <a href="${user.html_url}" target="blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+                </div>
+
+                <div class="col-md-9">
+                    <span class="badge bg-primary" >Public Repos: ${user.public_repos}</span>
+                    <span class="badge bg-secondary" >Public Gitsts: ${user.public_gists}</span>
+                    <span class="badge bg-success" >Followers: ${user.followers}</span>
+                    <span class="badge bg-info" >Following: ${user.following}</span>
+                    
+                    <br><br>
+
+                    <ul clas="list-group">
+                        <li class="list-group-item">Company: ${user.company}</li>
+                        <li class="list-group-item">Website/Blog: ${user.blog}</li>
+                        <li class="list-group-item">Location: ${user.location}</li>
+                        <li class="list-group-item">Account Created: ${user.created_at}</li>
+                    </ul>
+                </div>
+            </div>  
+        </div>
+        `;
+    }
+
+    clearProfile(){
+        this.profile.innerHTML = '';
+    }
+
+    showAlert(msg,className){
+        this.clearProfile();
+        this.clearAlert();
+        let div = document.createElement('div');
+
+        div.className = className;
+        div.appendChild(document.createTextNode(msg));
+
+        let container = document.querySelector('.searchContainer');
+        let search = document.querySelector('.serach');
+
+        container.insertBefore(div,search);
+
+    }
+    clearAlert(){
+        let currentAlert= document.querySelector('.alert');
+        if(currentAlert){
+            currentAlert.remove();
+        }
+    }
+}
